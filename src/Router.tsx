ee49eb06b34ebab316add22712ba52662e3b5a66
dominopiece <dp.dev.im@gmail.com>
2023-05-07 11:59:16 +0900

    :art: Tab Chart/Price

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,8 @@ import Root from "./Root";
 import Home from "./routes/Home";
 import Coins from "./routes/Coins";
 import Coin from "./routes/Coin";
+import Chart from "./routes/Chart";
+import Price from "./routes/Price";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +22,16 @@ const router = createBrowserRouter([
       {
         path: ":coinId",
         element: <Coin />,
+        children: [
+          {
+            path: "chart",
+            element: <Chart />,
+          },
+          {
+            path: "price",
+            element: <Price />,
+          },
+        ],
       },
     ],
   },
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -26,6 +27,55 @@ const Loader = styled.span`
   display: block;
   text-align: center;
   margin: 20px;
+`;
+
+const Img = styled.img`
+  height: 50px;
+  width: 50px;
+  margin: 10px;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  color: ${(props) => props.theme.textColor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 13px;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+  padding: 20px 15px;
+`;
+const LinkTabs = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 40px 80px;
+  text-align: center;
+  text-transform: uppercase;
+`;
+const LinkTab = styled.div<{ isActive: Boolean }>`
+  a {
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px;
+    border-radius: 10px;
+    color: ${(props) =>
+      props.isActive ? props.theme.accentColor : props.theme.textColor};
+  }
+  &:hover {
+    color: ${(props) => props.theme.accentColor};
+  }
 `;
 
 interface IRouteState {
@@ -108,23 +158,27 @@ function Coin() {
   // console.log(state?.name);
   const [info, setInfo] = useState<IInfo>();
   const [price, setPrice] = useState<IPrice>();
+  const chartMatch = useMatch("/:coinId/chart");
+  // console.log(chartMatch);
+  const priceMatch = useMatch("/:coinId/price");
+  // console.log(priceMatch);
 
   useEffect(() => {
     (async () => {
       const infoData = await (
         await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
       ).json();
-      console.log(infoData);
+      // console.log(infoData);
       setInfo(infoData);
 
       const priceData = await (
         await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
       ).json();
-      console.log(priceData);
+      // console.log(priceData);
       setPrice(priceData);
     })();
     setLoading(false);
-  }, []);
+  }, [coinId]);
 
   return (
     <>
@@ -132,8 +186,52 @@ function Coin() {
       <Container>
         <Header>
           <Title>Coin - {state?.name || "Loading.."}</Title>
+          <Title>
+            Coin - {state?.name ? state.name : loading ? "Loading" : info?.name}
+          </Title>
         </Header>
-        {loading ? <Loader>Loading..</Loader> : price?.quotes.USD.price}
+        {loading ? (
+          <Loader>Loading..</Loader>
+        ) : (
+          <>
+            <h1>babo coin</h1>
+            <Img src={info?.logo} alt={info?.name} />
+            <Overview>
+              <OverviewItem>
+                <span>rank 1</span>
+                <span>{info?.rank}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>span 2</span>
+                <span>span 2</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>span 3</span>
+                <span>span 3</span>
+              </OverviewItem>
+            </Overview>
+            <Description>{info?.description}</Description>
+            <Overview>
+              <OverviewItem>
+                <span>Price</span>
+                <span>$ {price?.quotes?.USD?.price.toFixed(2)} USD</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>span 2</span>
+                <span>span 2</span>
+              </OverviewItem>
+            </Overview>
+            <LinkTabs>
+              <LinkTab isActive={chartMatch !== null}>
+                <Link to="chart">Chart</Link>
+              </LinkTab>
+              <LinkTab isActive={priceMatch !== null}>
+                <Link to="price">Price</Link>
+              </LinkTab>
+            </LinkTabs>
+            <Outlet />
+          </>
+        )}
       </Container>
     </>
   );
